be728b106de7be3dd7ae3a818d820eb4611eb6a7
Aki <akiwainwright@gmail.com>
2023-08-06 10:57:27 +0900

    Setup base window

---
+++ SDL_Game/SDL_Game/consts.h
@@ -0,0 +1,8 @@
+#ifndef CONSTS_H
+#define CONSTS_H
+
+#define SCREENWIDTH 1280
+#define SCREENHEIGHT 720
+
+#endif // CONSTS_H
+
---
+++ SDL_Game/SDL_Game/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <SDL.h>
+#include <stdio.h>
+#include "consts.h"
+
+int main(int argc, char* args[])
+{
+	
+	//This is just a temporary window it will probably be changed
+	//Just wanted something to start with
+
+	SDL_Window* window = NULL;
+
+	SDL_Surface* screenSurface = NULL;
+
+	if (SDL_Init(SDL_INIT_VIDEO))
+	{
+		std::cout << "Failed to initilize SDL! SDL_error: " << SDL_GetError() << std::endl;
+	}
+	else
+	{
+		window = SDL_CreateWindow("SDL_Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREENWIDTH, SCREENHEIGHT, SDL_WINDOW_SHOWN);
+
+		if (!window)
+		{
+			std::cout << "Failed to create window! SDL_error: " << SDL_GetError() << std::endl; 
+		}
+		else
+		{
+			screenSurface = SDL_GetWindowSurface(window);
+
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+			SDL_UpdateWindowSurface(window);
+
+			SDL_Event e;
+
+			bool quit = false;
+
+			while (!quit)
+			{
+				while (SDL_PollEvent(&e))
+				{
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+				}
+			}
+		}
+
+		SDL_DestroyWindow(window);
+
+		SDL_Quit();
+	}
+
+
+
+	return 0;
+}

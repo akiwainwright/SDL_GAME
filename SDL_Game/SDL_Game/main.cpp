3ac9f940ebf76f836d1a3d431e63e7384f030e9c
Aki <akiwainwright@gmail.com>
2023-08-11 21:35:45 +0900

    Refactored code to get rid of indentations

--- SDL_Game/SDL_Game/main.cpp
+++ SDL_Game/SDL_Game/main.cpp
@@ -5,53 +5,48 @@
 
 int main(int argc, char* args[])
 {
-	
-	//This is just a temporary window it will probably be changed
-	//Just wanted something to start with
 	SDL_Window* window = NULL;
 
 	SDL_Surface* screenSurface = NULL;
 
+	SDL_Event e;
+
+	bool running = true;
+
 	if (SDL_Init(SDL_INIT_VIDEO))
 	{
 		std::cout << "Failed to initilize SDL! SDL_error: " << SDL_GetError() << std::endl;
+
+		return 1;
 	}
-	else
+
+	if (!(window = SDL_CreateWindow("SDL_Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREENWIDTH, SCREENHEIGHT, SDL_WINDOW_SHOWN)))
 	{
-		window = SDL_CreateWindow("SDL_Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREENWIDTH, SCREENHEIGHT, SDL_WINDOW_SHOWN);
+		std::cout << "Failed to create window! SDL_error: " << SDL_GetError() << std::endl;
 
-		if (!window)
+		return 1;
+	}
+
+	screenSurface = SDL_GetWindowSurface(window);
+
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
+
+	SDL_UpdateWindowSurface(window);
+
+	while (running)
+	{
+		while (SDL_PollEvent(&e))
 		{
-			std::cout << "Failed to create window! SDL_error: " << SDL_GetError() << std::endl; 
-		}
-		else
-		{
-			screenSurface = SDL_GetWindowSurface(window);
-
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
-
-			SDL_UpdateWindowSurface(window);
-
-			SDL_Event e;
-
-			bool quit = false;
-
-			while (!quit)
+			if (e.type == SDL_QUIT)
 			{
-				while (SDL_PollEvent(&e))
-				{
-					if (e.type == SDL_QUIT)
-					{
-						quit = true;
-					}
-				}
+				running = false;
 			}
 		}
-
-		SDL_DestroyWindow(window);
-
-		SDL_Quit();
 	}
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
 
 	return 0;
 }
